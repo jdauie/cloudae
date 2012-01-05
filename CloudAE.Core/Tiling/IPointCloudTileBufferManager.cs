731312ffc7b4966f97599f30e969f7b40a42e327
Joshua Morey <jdauie@gmail.com>
2012-01-05 03:07:47 +0000

    Moving code around

--- CloudAE.Core/Tiling/IPointCloudTileBufferManager.cs
+++ CloudAE.Core/Tiling/IPointCloudTileBufferManager.cs
@@ -7,47 +7,6 @@ using System.IO;
 
 namespace CloudAE.Core
 {
-	public enum PointCloudTileBufferManagerMode
-	{
-		None,
-		OptimizeForLargeFile
-	}
-
-	public class PointCloudTileBufferManagerOptions
-	{
-		public readonly PointCloudTileBufferManagerMode Mode;
-		public readonly FileOptions TilingFileOptions;
-		public readonly bool AllowSparseAllocation;
-
-		public PointCloudTileBufferManagerOptions(PointCloudTileBufferManagerMode mode)
-		{
-			Mode = mode;
-
-			if (Mode == PointCloudTileBufferManagerMode.OptimizeForLargeFile)
-			{
-				TilingFileOptions = FileOptions.SequentialScan | FileOptions.WriteThrough;
-				AllowSparseAllocation = true;
-			}
-			else
-			{
-				TilingFileOptions = FileOptions.RandomAccess | FileOptions.WriteThrough;
-				AllowSparseAllocation = false;
-			}
-		}
-
-		public IPointCloudTileBufferManager CreateManager(PointCloudTileSource tileSource, FileStream outputStream)
-		{
-			IPointCloudTileBufferManager tileBufferManager;
-
-			if (Mode == PointCloudTileBufferManagerMode.OptimizeForLargeFile)
-				tileBufferManager = new PointCloudTileBufferManager2(tileSource, outputStream);
-			else
-				tileBufferManager = new PointCloudTileBufferManager(tileSource, outputStream);
-
-			return tileBufferManager;
-		}
-	}
-
 	public interface IPointCloudTileBufferManager
 	{
 		/// <summary>
