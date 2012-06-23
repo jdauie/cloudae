171b5711715e571dde0a7f05b4b7447588dc169a
Joshua Morey <jdauie@gmail.com>
2012-06-23 01:07:23 +0000

    Cleanup

--- CloudAE.Core/Sources/IPointCloudBinarySourceEnumerable.cs
+++ CloudAE.Core/Sources/IPointCloudBinarySourceEnumerable.cs
@@ -1,17 +1,16 @@
 ﻿using System;
 
-using CloudAE.Core.Geometry;
-
 namespace CloudAE.Core
 {
 	public interface IPointCloudBinarySourceEnumerable
 	{
-		string FilePath { get; }
-		long Count { get; }
-		long PointDataOffset { get; }
-		short PointSizeBytes { get; }
-		int UsableBytesPerBuffer { get; }
-		int PointsPerBuffer { get; }
+		string FilePath             { get; }
+		long   Count                { get; }
+		long   PointDataOffset      { get; }
+		short  PointSizeBytes       { get; }
+		int    UsableBytesPerBuffer { get; }
+		int    PointsPerBuffer      { get; }
+
 		PointCloudBinarySourceEnumerator GetBlockEnumerator(byte[] buffer);
 	}
 }
