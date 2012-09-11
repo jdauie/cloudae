445a37d2a487463709f258f724acadeda19326ab
Joshua Morey <jdauie@gmail.com>
2012-09-11 05:26:59 +0000

    Added stub for cache manager.

---
+++ CloudAE.Core/Managers/PointCloudTileCacheManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CloudAE.Core
+{
+	public class PointCloudTileCache
+	{
+	}
+
+	/// <summary>
+	/// Manages the memory for all tile caches.
+	/// Do I only allow one source to have an active cache at a time?  
+	/// I'm thinking not, for more flexibility...like loading multiple sources in a viewer.
+	/// </summary>
+	public class PointCloudTileCacheManager
+	{
+		
+	}
+}
