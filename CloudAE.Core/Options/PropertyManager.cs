c1cdc5d9333bb941fffff1b1c7f49790e0d2c677
Joshua Morey <jdauie@gmail.com>
2012-10-31 04:38:56 +0000

    Cleanup

--- CloudAE.Core/Options/PropertyManager.cs
+++ CloudAE.Core/Options/PropertyManager.cs
@@ -2,7 +2,6 @@
 using System.IO;
 using System.Linq;
 using System.Reflection;
-using Microsoft.Win32;
 
 namespace CloudAE.Core
 {
--- CloudAE.Core/Options/RegistryPropertyManager.cs
+++ CloudAE.Core/Options/RegistryPropertyManager.cs
@@ -5,17 +5,6 @@ using Microsoft.Win32;
 
 namespace CloudAE.Core
 {
-	public interface IPropertyManager
-	{
-		PropertyName CreatePropertyName(string name);
-		PropertyName CreatePropertyName(string prefix, string name);
-		IPropertyState<T> Create<T>(PropertyName name, T defaultValue);
-		bool SetProperty(PropertyName name, ISerializeStateBinary value);
-		bool GetProperty(PropertyName name, ISerializeStateBinary value);
-		bool SetProperty(IPropertyState state);
-		bool GetProperty(IPropertyState state);
-	}
-
 	public class RegistryPropertyManager : IPropertyManager
 	{
 		public IPropertyState<T> Create<T>(PropertyName propertyName, T defaultValue)
