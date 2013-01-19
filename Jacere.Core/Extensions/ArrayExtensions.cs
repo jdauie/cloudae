cb064271a8837e1700b18c68ce0234ad9884a0da
Joshua Morey <jdauie@gmail.com>
2013-01-19 03:56:48 +0000

    Added extensions, removed problematic QuantizationTest/Converter

---
+++ Jacere.Core/Extensions/ArrayExtensions.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jacere.Core
+{
+	public static class ArrayExtensions
+	{
+		public static IEnumerable<T> ToEnumerable<T>(this Array target)
+		{
+			foreach (var item in target)
+				yield return (T)item;
+		}
+
+		public static string ToAsciiString(this byte[] buffer)
+		{
+			int nullLocation = Array.IndexOf<byte>(buffer, 0);
+			if (nullLocation > -1)
+				return Encoding.ASCII.GetString(buffer, 0, nullLocation);
+			else
+				return Encoding.ASCII.GetString(buffer);
+		}
+
+		public static void ParallelSort(this int[] target, int count)
+		{
+			const int bucketCountPow = 2;
+			int bucketCount = (int)Math.Pow(2, bucketCountPow);
+
+			// get range for shifting
+			int min = target[0];
+			int max = target[0];
+			for (int i = 0; i < count; i++)
+				if (target[i] < min) min = target[i]; else if (target[i] > max) max = target[i];
+			long range = (long)max - min;
+			int rangePowCeil = (int)Math.Ceiling(Math.Log(range, 2));
+			int bucketCountShift = rangePowCeil - bucketCountPow;
+			int minShifted = min >> bucketCountShift;
+
+			// determine bucket sizes
+			int[] bucketCounts = new int[bucketCount + 1];
+			for (int i = 0; i < count; i++)
+				++bucketCounts[(target[i] >> bucketCountShift) - minShifted];
+
+			int[][] buckets = new int[bucketCount + 1][];
+			for (int b = 0; b < buckets.Length; b++)
+				buckets[b] = new int[bucketCounts[b]];
+
+			int[] bucketPositions = new int[bucketCount + 1];
+
+			// copy points to buckets
+			for (int i = 0; i < count; i++)
+			{
+				int bucket = (target[i] >> bucketCountShift) - minShifted;
+				buckets[bucket][bucketPositions[bucket]++] = target[i];
+			}
+
+			Parallel.ForEach(buckets.Where(b => b.Length > 0), Array.Sort);
+
+			// copy back
+			int position = 0;
+			for (int b = 0; b < buckets.Length; b++)
+			{
+				Array.Copy(buckets[b], 0, target, position, buckets[b].Length);
+				position += buckets[b].Length;
+			}
+		}
+	}
+
+}
---
+++ Jacere.Core/Extensions/GeometryExtensions.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.IO;
+
+using Jacere.Core.Geometry;
+
+namespace Jacere.Core
+{
+	public static class GeometryExtensions
+	{
+		public static Point3D ReadPoint3D(this BinaryReader reader)
+		{
+			return new Point3D(reader);
+		}
+
+		public static Extent3D ReadExtent3D(this BinaryReader reader)
+		{
+			return new Extent3D(reader);
+		}
+
+		public static UQuantizedExtent3D ReadUQuantizedExtent3D(this BinaryReader reader)
+		{
+			return new UQuantizedExtent3D(reader);
+		}
+
+		public static UQuantizedPoint3D ReadUQuantizedPoint3D(this BinaryReader reader)
+		{
+			return new UQuantizedPoint3D(reader);
+		}
+
+		public static SQuantizedExtent3D ReadSQuantizedExtent3D(this BinaryReader reader)
+		{
+			return new SQuantizedExtent3D(reader);
+		}
+
+		public static SQuantizedPoint3D ReadSQuantizedPoint3D(this BinaryReader reader)
+		{
+			return new SQuantizedPoint3D(reader);
+		}
+
+		public static UQuantization3D ReadUQuantization3D(this BinaryReader reader)
+		{
+			return new UQuantization3D(reader);
+		}
+
+		public static SQuantization3D ReadSQuantization3D(this BinaryReader reader)
+		{
+			return new SQuantization3D(reader);
+		}
+	}
+}
