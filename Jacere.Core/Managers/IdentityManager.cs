4ac8683f42afe4544ea02f52d257002a298a96c2
Joshua Morey <jdauie@gmail.com>
2013-01-19 02:42:39 +0000

    Adding buffer-related code to Jacere.Core

---
+++ Jacere.Core/Managers/IdentityManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CloudAE.Core
+{
+	public static class IdentityManager
+	{
+#warning this list is just for debugging reasons at this point
+		private static List<Identity> m_identities;
+
+		private static uint m_nextIncrementalValue;
+
+		static IdentityManager()
+		{
+			m_identities = new List<Identity>();
+
+			m_nextIncrementalValue = 1;
+		}
+
+		public static Identity AcquireIdentity(string name)
+		{
+			return AcquireIdentity(name, IdentityType.Unknown);
+		}
+
+		public static Identity AcquireIdentity(string name, IdentityType type)
+		{
+			Identity id = new Identity(m_nextIncrementalValue, name, type);
+			m_identities.Add(id);
+
+			++m_nextIncrementalValue;
+
+			return id;
+		}
+	}
+}
