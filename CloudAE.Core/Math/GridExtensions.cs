3615077909b6cfc58ed55204e97893b3059ee1fb
Joshua Morey <jdauie@gmail.com>
2013-03-07 06:47:59 +0000

    Added back the CorrectOverflow for indexing temporarily, just to make it compile.

---
+++ CloudAE.Core/Math/GridExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using Jacere.Core;
+
+namespace CloudAE.Core
+{
+	public static class GridExtensions
+	{
+		public static void CorrectCountOverflow(this Grid<GridIndexCell> target)
+		{
+			var data = target.Data;
+
+			// correct count overflows
+			for (int x = 0; x <= target.SizeX; x++)
+			{
+				data[target.SizeY - 1, x] = new GridIndexCell(data[target.SizeY - 1, x], data[target.SizeY, x]);
+				data[target.SizeY, x] = new GridIndexCell();
+			}
+			for (int y = 0; y < target.SizeY; y++)
+			{
+				data[y, target.SizeX - 1] = new GridIndexCell(data[y, target.SizeX - 1], data[y, target.SizeX]);
+				data[y, target.SizeX] = new GridIndexCell();
+			}
+		}
+	}
+}
