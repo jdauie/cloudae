8bfa9900ccf81be3e6b0a788ca5872941c2d290d
Joshua Morey <jdauie@gmail.com>
2013-03-13 00:41:05 +0000

    Fixed Indexing.

--- CloudAE.Core/Math/GridIndexGenerator.cs
+++ CloudAE.Core/Math/GridIndexGenerator.cs
@@ -28,16 +28,14 @@ namespace CloudAE.Core
 			var regionSourcesBySegment = new List<List<Range>>();
 			var tilesPerSegment = new List<GridRange>();
 			long pointDataBytes = m_source.PointSizeBytes * m_source.Count;
-			long pointDataRemainingBytes = pointDataBytes;
-
+			
 			int chunksPerSegment = segmentSize / (m_maxPointCountPerChunk * m_source.PointSizeBytes);
 
 			var tileOrder = PointCloudTileSet.GetTileOrdering(m_actualGrid.SizeY, m_actualGrid.SizeX).ToArray();
 			int tileOrderIndex = 0;
-			while (pointDataRemainingBytes > 0)
+			while (tileOrderIndex < tileOrder.Length)
 			{
-				var cellList = new HashSet<int>();
-				int currentSize = 0;
+				var segmentChunks = new HashSet<int>();
 
 				var startTile = tileOrder[tileOrderIndex];
 				var startTileIndex = new GridCoord(m_actualGrid.Def, startTile.Row, startTile.Col);
@@ -49,63 +47,45 @@ namespace CloudAE.Core
 					// unfortunately, I cannot check the actual counts, because they have not been measured
 					// instead, I can count the estimated area, and undershoot
 
-#warning THIS NEEDS TO CHECK ONLY THE NEW CELL COUNTS
-					// currentSize is counting points repeatedly!
+					// get unique chunks
+					var correspondingEstimatedIndex = m_estimatedIndex.GetCellsInScaledRange(tile.Col, tile.Row, m_actualGrid).ToList();
+					var uniqueChunks = correspondingEstimatedIndex.SelectMany(c => c.Chunks).ToHashSet(segmentChunks);
 
-					var count = m_estimatedCounts.GetCellsInScaledRange(tile.Col, tile.Row, m_actualGrid).Sum();
-					if (count > 0)
-					{
-						int tileSize = (count * m_source.PointSizeBytes);
-						if (currentSize + tileSize > segmentSize)
-							break;
+					// this is safe, but it can create smaller segments than necessary
+					if (segmentChunks.Count + uniqueChunks.Count > chunksPerSegment)
+						break;
 
-						// get unique chunks
-						var cellList2 = new HashSet<int>();
-						var correspondingEstimatedIndex = m_estimatedIndex.GetCellsInScaledRange(tile.Col, tile.Row, m_actualGrid).ToList();
-						foreach (var estimatedIndex in correspondingEstimatedIndex)
-							foreach (var index in estimatedIndex.Chunks)
-								if (!cellList.Contains(index))
-									cellList2.Add(index);
-
-						// undershoot again by counting the data read, not just the valid points
-						// I really only need to do this for testing -- it slows things down a bit.
-						// Once I am sure that filtering is working properly, this won't matter.
-#warning remove this once I'm sure
-						//if (cellList.Count + cellList2.Count > chunksPerSegment)
-						//    break;
-
-						foreach (var index in cellList2)
-							cellList.Add(index);
-
-						currentSize += tileSize;
-					}
+					// merge
+					foreach (var index in uniqueChunks)
+						segmentChunks.Add(index);
 
 					++tileOrderIndex;
 				}
 
-
-				var endTile = tileOrder[tileOrderIndex - 1];
-				var endTileIndex = new GridCoord(m_actualGrid.Def, endTile.Row, endTile.Col);
-
-				// group by sequential regions
-				int[] sortedCellList = cellList.ToArray();
-				Array.Sort(sortedCellList);
-				var regions = new List<Range>();
-				int sequenceStartIndex = 0;
-				while (sequenceStartIndex < sortedCellList.Length)
+				if (segmentChunks.Count > 0)
 				{
-					// find incremental sequence
-					int i = sequenceStartIndex;
-					++i;
-					while (i < sortedCellList.Length && (sortedCellList[i] == sortedCellList[i - 1] + 1))
-						++i;
-					regions.Add(new Range(sortedCellList[sequenceStartIndex], i - sequenceStartIndex));
-					sequenceStartIndex = i;
-				}
+					var endTile = tileOrder[tileOrderIndex - 1];
+					var endTileIndex = new GridCoord(m_actualGrid.Def, endTile.Row, endTile.Col);
 
-				regionSourcesBySegment.Add(regions);
-				tilesPerSegment.Add(new GridRange(startTileIndex, endTileIndex));
-				pointDataRemainingBytes -= currentSize;
+					// group by sequential regions
+					int[] sortedCellList = segmentChunks.ToArray();
+					Array.Sort(sortedCellList);
+					var regions = new List<Range>();
+					int sequenceStartIndex = 0;
+					while (sequenceStartIndex < sortedCellList.Length)
+					{
+						// find incremental sequence
+						int i = sequenceStartIndex;
+						++i;
+						while (i < sortedCellList.Length && (sortedCellList[i] == sortedCellList[i - 1] + 1))
+							++i;
+						regions.Add(new Range(sortedCellList[sequenceStartIndex], i - sequenceStartIndex));
+						sequenceStartIndex = i;
+					}
+
+					regionSourcesBySegment.Add(regions);
+					tilesPerSegment.Add(new GridRange(startTileIndex, endTileIndex));
+				}
 			}
 
 			int chunkRangeSumForAllSegments = regionSourcesBySegment.Sum(r => r.Sum(r2 => r2.Count));
---
+++ Jacere.Core/Extensions/HashSetExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Jacere.Core
+{
+	public static class HashSetExtensions
+	{
+		public static HashSet<T> ToHashSet<T>(this IEnumerable<T> input)
+		{
+			var set = new HashSet<T>();
+			foreach (var value in input)
+				set.Add(value);
+
+			return set;
+		}
+
+		public static HashSet<T> ToHashSet<T>(this IEnumerable<T> input, HashSet<T> except)
+		{
+			var set = input.ToHashSet();
+			set.ExceptWith(except);
+
+			return set;
+		}
+	}
+}
